69e8726a093659086710b346070c5ad4878514f9
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-17 22:59:18 -0500

    Lab05 6 Numeros de Telefono

--- telefonos/src/App.js
+++ telefonos/src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const App = () => {
   //componentes
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "39-44-5323523" }
   ]) 
 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const nuevoNombre = (event) =>{
     setNewName(event.target.value)
@@ -17,7 +18,7 @@ const App = () => {
     if(nombres.includes(newName)){
       window.alert(`${newName} Ya existe en el phonebook`)
     } else {
-      setPersons(persons.concat({name:newName}))
+      setPersons(persons.concat({name:newName,number:newNumber}))
     }
     event.preventDefault()
   }
@@ -28,7 +29,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={personas}>
         <div>
-          Nombre : <input onChange={nuevoNombre}/>
+          Nombre : <input onChange={(event)=>{setNewName(event.target.value)}}/><br/>
+          Numero : <input onChange={(event)=>{setNewNumber(event.target.value)}}/>
         </div>
         <div>
           <button type="submit" >add</button>
@@ -36,7 +38,7 @@ const App = () => {
       </form>
       <h2>Numeros</h2>
       {persons.map((value)=>{
-        return <div>{value.name}</div>
+        return <Fragment><div>Nombre: {value.name} <br/>Numero: {value.number}</div><br/></Fragment>
       })}
     </div>
   )
