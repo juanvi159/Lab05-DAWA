2522cde50c29df722b0769026f17ef5756b7be1d
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-17 22:18:33 -0500

    primer commit

--- telefonos/src/App.js
+++ telefonos/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [ newName, setNewName ] = useState('')
+
+  const nuevoNombre = (event) =>{
+    setNewName(event.target.value)
+  }
+
+  const personas = (event) =>{
+    setPersons(persons.concat({name:newName}))
+    event.preventDefault()
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={personas}>
+        <div>
+          Nombre : <input onChange={nuevoNombre}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numeros</h2>
+      {persons.map((value)=>{
+        return <div>{value.name}</div>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
