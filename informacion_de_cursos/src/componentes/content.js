20f5760cd5bfb6e429b1f3e118fdd110e990a162
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-04-17 21:18:50 -0500

    Lab05 2 Suma de los ejercicios

--- informacion_de_cursos/src/componentes/content.js
+++ informacion_de_cursos/src/componentes/content.js
@@ -2,16 +2,24 @@ import React, { Fragment } from 'react';
 import Part from './part';
 
 export default function Content({parts}){
+    
+    var ejer = []
+    for (var part in parts){
+        ejer.push(parts[part].exercises)
+    }
+    const total = ejer.reduce((s, p) => s+p)
+
     return(
         <div>
             {parts.map((value,index) => {
                 return (
                     <Fragment>
-                        <h3>Parte {index+1}</h3>
+                        <h3>Parte: {index+1}</h3>
                         <Part part={value}/>
                     </Fragment>
                 )
             })}
+            <h2>Total de ejercicios: {total}</h2>
         </div>
     );
 } 
